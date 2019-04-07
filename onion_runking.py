0d9dfe2e56359f8b353fdeaaf5bcf37573e2bc9e
famasoon <famasoon@gmail.com>
2019-04-07 23:02:03 +0900

    Add onion_rankng

---
+++ onion_runking.py
@@ -0,0 +1,17 @@
+import urllib.request
+import json
+import sys
+
+# arg e.g. https://tor2web.io/ https://onion.to
+
+TOR2WEB_DATA_URL = '/antanistaticmap/stats/yesterday'
+
+req = urllib.request.Request(sys.argv[1] + TOR2WEB_DATA_URL)
+with urllib.request.urlopen(req) as res:
+    body = json.loads(res.read().decode('utf-8'))
+
+services = body['hidden_services']
+services.sort(key=lambda x: x['access_count'])
+services.reverse()
+for service in services:
+    print("URL: {}.onion , Count: {}".format(service['id'], service['access_count']))
